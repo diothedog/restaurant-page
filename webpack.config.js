b82bd239e36d81c5339da78d395c40b594379437
diothedog <88989273+diothedog@users.noreply.github.com>
2024-07-19 15:40:57 -0500

    Hard code homepage content into html file

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
